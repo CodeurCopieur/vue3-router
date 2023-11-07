ec37b81323b2df87b4ff432cea86d3d0b6d4549e
Widdy LOUIS <louisws@outlook.fr>
2023-11-08 00:20:13 +0100

    use unocss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import 'uno.css'
 import './style.css'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -7,6 +8,7 @@ const router = createRouter({
   routes: [
     {path: '/', component: () => import('./pages/Home.vue')},
     {path: '/about', component: () => import('./pages/About.vue')},
+    {path: '/posts', component: () => import('./pages/Posts.vue')},
     {path: '/:pathMatch(.*)', component: () => import('./pages/NotFound.vue')}
   ],
   history: createWebHistory(import.meta.env.BASE_URL),
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import UnoCss from 'unocss/vite'
+import {presetAttributify, presetUno} from 'unocss'
+
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), UnoCss([{ presets: [presetUno(), presetAttributify()] }])],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
