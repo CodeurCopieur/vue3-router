99bb247a9686b57c56c1946b13e59effb91afb89
Widdy LOUIS <louisws@outlook.fr>
2023-11-08 14:31:11 +0100

    fetch posts Data

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,11 +1,35 @@
 <script setup>
-  
+  import { ref, onMounted } from 'vue'
+
+  const posts = ref([]);
+
+  onMounted(()=> {
+    fetchPosts()
+  });
+
+  const fetchPosts = async()=> {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    posts.value = await res.json()
+  };
 </script>
 
 <template>
  <h1 class="text-black">Posts</h1>
-</template>
 
-<style lang="scss" scoped>
-  
-</style>
+ <table class="table-responsive" v-if="posts.length">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Body</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="post in posts" :key="post.id">
+                <td data-label="Nom">{{ post.id}}</td>
+                <td data-label="Title">{{ post.title}}</td>
+                <td data-label="Body">{{ post.body}}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
