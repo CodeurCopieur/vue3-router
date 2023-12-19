562aa5a484446177d31717124621be044bb3ef17
Codwerk <36073831+CodeurCopieur@users.noreply.github.com>
2023-12-19 21:15:46 +0100

    end : Syntaxe de correspondance de route utilisant le modèle Regex dans l'API de composition de routes

--- src/pages/ProductComponent.vue
+++ src/pages/ProductComponent.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { useRoute } from "vue-router";
+  const route = useRoute()
+</script>
 <template>
-  <h1>Product component</h1>
+  <div class="text-black">
+    <h1>Product component</h1>
+    <p>{{ route.params }}</p>
+  </div>
 </template>
